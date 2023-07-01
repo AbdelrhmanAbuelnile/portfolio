7229f9f3a74e3a33d827f26f0e310ee80db6d968
AbdelrhmanAbuelnile <abdohani362@gmail.com>
2023-07-01 17:51:10 +0300

    final touches

--- components/About.vue
+++ components/About.vue
@@ -21,7 +21,7 @@
             
             </div>
         
-          <div class="lg:w-3/5">
+          <div class="lg:w-4/5">
               <img
                   src="https://images.unsplash.com/photo-1550745165-9bc0b252726f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
                   alt=""
--- components/Footer.vue
+++ components/Footer.vue
@@ -1,13 +1,13 @@
 <template>
-    <footer class="flex flex-col justify-center items-start text-gray-200 lg:px-10 px-2">
+    <footer class="flex flex-row justify-between items-center text-gray-200 lg:px-10 px-2">
         
         <div>
             <p class="md:text-3xl text-lg font-bold mb-8">Copyright © {{ currentYear }} | <a href="https://www.linkedin.com/in/abdelrahman-hany-abu-elnile" class="hover:text-gray-600" target='_blank'>Abdelrhman</a> </p>
         </div>
 
-        <div class="flex flex-row justify-between items-center w-12">
+        <div class="flex flex-row justify-between items-center w-fit px-4">
             <a :href="icons.url" target='_blank' v-for="icons in toolsIcon" :key="icons.name">
-                <Icon :name="icons.name" class="w-10 h-10 mx-2 text-slate-900" />
+                <Icon :name="icons.name" :class="`w-14 h-10 mx-2 text-slate-400 hover:text-[${icons.color}] duration-300`" />
             </a>
         </div>
     </footer>
@@ -21,16 +21,19 @@ const currentYear = ref(currentDate.value.getFullYear());
 
 const toolsIcon = [
         {
-            name: 'logos:github-icon',
-            url: 'https://github.com/AbdelrhmanAbuelnile'
-        },
-        {
             name: 'uil:linkedin',
-            url: 'https://www.linkedin.com/in/abdelrahman-hany-abu-elnile'
+            url: 'https://www.linkedin.com/in/abdelrahman-hany-abu-elnile',
+            color:'#0072b1'
         },
         {
-            name: 'logos:upwork',
-            url: 'https://www.upwork.com/freelancers/~01e19cc3af51978fc6'
+            name: 'uil:github',
+            url: 'https://github.com/AbdelrhmanAbuelnile',
+            color: '#333'
+        },
+        {
+            name: 'bxl:upwork',
+            url: 'https://www.upwork.com/freelancers/~01e19cc3af51978fc6',
+            color: '#6fda44'
         },
     ]
 </script>
@@ -41,6 +44,6 @@ const toolsIcon = [
         background-repeat: no-repeat;
         background-size: cover;
         width: 100%;
-        height: 500px;
+        height: 300px;
     }
 </style>
